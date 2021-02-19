456333769dcf8ab1349cfbf270a7249f50ed05cd
MattMrak <matthewmrakovcic@gmail.com>
2021-02-19 11:28:18 -0500

    commit tree b89c133504319c987b8ac15a410aa75804da6bd2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
+  resources :genres
+  resources :artists
+  resources :songs
+  # get 'genres/name'
+
+  # get 'artists/name'
+
+  # get 'artists/bio'
+
+  # get 'songs/name'
+
+  # get 'songs/artist_id'
+
+  # get 'songs/genre_id'
+
+  # get 'artist/name'
+
+  # get 'artist/bio'
+
+  # get 'genre/name'
+
+  # get 'admin/dashboard'
+
+  # get 'admin/stats'
+
+  # get 'admin/financials'
+
+  # get 'admin/settings'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
